b1611e88f2f9723168cfcfef12d4fe596df99917
Harsh-Vardhan17Singh <harshatwork171827@gmail.com>
2026-07-22 23:17:42 +0530

    Adding user.py in models

---
+++ backend/app/models/user.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String
+
+from app.db.base import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    full_name = Column(String, nullable=False)
+
+    email = Column(String, unique=True, index=True)
+
+    password = Column(String)
+
+    role = Column(String, default="analyst")
