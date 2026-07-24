51b532caf1cd86e0ad1154429e1d0f72276671e6
Harsh-Vardhan17Singh <harshatwork171827@gmail.com>
2026-07-24 23:06:22 +0530

    Modifying main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
+
 from app.api.router import api_router
+from app.db.init_db import create_tables
 
 app = FastAPI(
     title="Panopticon API",
     version="1.0.0",
-    description="Enterprise Fraud Intelligence Platform"
 )
+
+create_tables()
 
 app.include_router(api_router)
 
