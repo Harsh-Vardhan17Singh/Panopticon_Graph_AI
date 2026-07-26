3d79a345498e02f6d0c0b4a9a664236dc735c820
Harsh-Vardhan17Singh <harshatwork171827@gmail.com>
2026-07-26 23:26:01 +0530

    Updating every API to get its own file

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,13 +1,9 @@
 from fastapi import APIRouter
 
+from app.api.health import router as health_router
+
 api_router = APIRouter(
     prefix="/api/v1"
 )
 
-
-@api_router.get("/health")
-def health():
-    return {
-        "status": "healthy",
-        "service": "Panopticon Backend"
-    }
+api_router.include_router(health_router)
