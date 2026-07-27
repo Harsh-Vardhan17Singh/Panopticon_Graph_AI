0105758da8f6bedb695152820d0cc6e76a92463e
Harsh-Vardhan17Singh <harshatwork171827@gmail.com>
2026-07-27 23:26:41 +0530

    Updating config.py for a better flow

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,8 +2,16 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+
     APP_NAME: str = "Panopticon"
+
     VERSION: str = "1.0.0"
+
+    DEBUG: bool = True
+
+    API_PREFIX: str = "/api/v1"
+
+    DATABASE_URL: str = "sqlite:///./panopticon.db"
 
     class Config:
         env_file = ".env"
