89a6a60021a8874a60d4507ba082e9cc7b623963
Harsh-Vardhan17Singh <harshatwork171827@gmail.com>
2026-07-30 23:58:32 +0530

    Adding fraud model device

---
+++ backend/app/models/device.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from app.db.base import Base
+
+
+class Device(Base):
+    __tablename__ = "devices"
+
+    id = Column(Integer, primary_key=True, index=True)
+    device_id = Column(String, unique=True, nullable=False)
+    device_type = Column(String)
+    operating_system = Column(String)
+    ip_address = Column(String)
