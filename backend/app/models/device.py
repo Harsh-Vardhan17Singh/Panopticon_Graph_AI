4f146b417d6d76febea9cdbcd84007a64866c877
Harsh-Vardhan17Singh <harshatwork171827@gmail.com>
2026-08-03 22:58:40 +0530

    feat(backend):Updating file for transaction and Auth

--- backend/app/models/device.py
+++ backend/app/models/device.py
@@ -6,7 +6,16 @@ class Device(Base):
     __tablename__ = "devices"
 
     id = Column(Integer, primary_key=True, index=True)
+
     device_id = Column(String, unique=True, nullable=False)
+
     device_type = Column(String)
+
     operating_system = Column(String)
+
     ip_address = Column(String)
+
+    transactions = relationship(
+        "Transaction",
+        back_populated="device"
+    )
