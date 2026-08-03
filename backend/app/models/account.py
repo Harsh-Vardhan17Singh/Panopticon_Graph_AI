d44a9270621ba02165ddc794167a17bc4cad8159
Harsh-Vardhan17Singh <harshatwork171827@gmail.com>
2026-08-03 22:50:35 +0530

    Updating account backend file

--- backend/app/models/account.py
+++ backend/app/models/account.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+
 from app.db.base import Base
 
 
@@ -6,7 +8,27 @@ class Account(Base):
     __tablename__ = "accounts"
 
     id = Column(Integer, primary_key=True, index=True)
+
     account_number = Column(String, unique=True, nullable=False)
+
     account_type = Column(String, nullable=False)
+
     balance = Column(Float, default=0.0)
-    status = Column(String, default="ACTIVE")
+
+    status = Column(String, default="ACTIVE",nullable=False)
+
+    organization_id = Column(
+        Integer,
+        ForeignKey("organizations.id"),
+        nullable=False
+    )
+
+    organization = relationship(
+        "Organization",
+        back_populates="accounts"
+    )
+
+    transactions = relationship(
+        "Transaction",
+        back_populates="account"
+    )
