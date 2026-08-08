538dffaa16fb867fce3f70c2f5fbdcee8f4a05aa
Harsh-Vardhan17Singh <harshatwork171827@gmail.com>
2026-08-08 23:57:32 +0530

    Updating API router file

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,9 +1,14 @@
 from fastapi import APIRouter
 
 from app.api.health import router as health_router
+from app.api.transaction import router as transaction_router
+
 
 api_router = APIRouter(
     prefix="/api/v1"
 )
 
+
 api_router.include_router(health_router)
+
+api_router.include_router(transaction_router)
