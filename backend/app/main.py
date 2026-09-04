7e804af153c4eb5856d54d308ed8cdda183c71f0
Harsh-Vardhan17Singh <harshatwork171827@gmail.com>
2026-09-04 13:30:56 +0530

    fix Production cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,14 +24,17 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+
+        # Vercel production URL
         "https://panopticon-graphai.vercel.app",
+
+        # Vercel deployment URL currently being used
+        "https://panopticon-graph-ai-git-main-harsh-vardhan17singh-projects.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
 app.include_router(api_router)
 
 
